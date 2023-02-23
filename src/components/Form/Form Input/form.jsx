8d8616c0de61e3348bc98c1631b5e4416f8fb0b9
RIZQI NUR RIFA'I <84585203+rifai-rizqi3@users.noreply.github.com>
2023-02-23 12:47:23 +0700

    update

---
+++ src/components/Form/Form Input/form.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const ShowError = ({ errors }) => {
+    return (
+        <ul style={{color: 'red', marginLeft: '-20px'}}>
+            {
+                errors.map((error, i) => <li key={i}>{error}</li>)
+            }
+        </ul>)
+
+}
+
+export default class Form extends React.Component {
+
+    state = {
+        nama: "",
+        email: "",
+        nomor: "",
+        kelamin: "",
+        password: "",
+        setuju: false,
+        errors: []
+    }
+
+    handleRegister = (e) => {
+        e.preventDefault();
+        const { nama, email, nomor, kelamin, password } = this.state;
+
+        let massage = [];
+
+        if (nama.length === 0) {
+            massage = [...massage, "nama tidak boleh kosong"]
+        }
+
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (email.length === 0) {
+            massage = [...massage, "email tidak boleh kosong"]
+        } else if(!re.test(String(email).toLocaleLowerCase())) {
+            massage = [...massage, "Email tidak valid"]
+        }
+
+        if (nomor.length === 0) {
+            massage = [...massage, "nomor tidak boleh kosong"]
+        }
+        if (kelamin.length === 0) {
+            massage = [...massage, "jenis kelamin tidak boleh kosong"]
+        }
+        if (password.length === 0) {
+            massage = [...massage, "password tidak boleh kosong"]
+        } else if(password.length < 8) {
+            massage = [...massage, "Password terlalu pendek"]
+        }
+
+        
+        
+
+        
+
+        if (massage.length > 0) {
+            this.setState({
+                errors: massage
+            },);
+        } else {
+            alert(`
+        nama: ${this.state.nama}
+        email: ${this.state.email}
+        nomor: ${this.state.nomor}
+        kelamin: ${this.state.kelamin}
+        password: ${this.state.password}
+        setuju: ${this.state.setuju ? "YES" : "NO"}
+        `)
+        this.setState({
+            nama: "",
+            email: "",
+            nomor: "",
+            kelamin: "",
+            password: "",
+            setuju: false,
+            errors: []
+        })
+        }
+
+        
+    }
+
+    render() {
+        return (
+            <>
+            <div class="container justify-content-center w-50 p-5">
+            {
+                this.state.errors && <ShowError errors={this.state.errors} />
+            }
+                <h2>Form Register</h2>
+                <form class="mt-3" onSubmit={this.handleRegister}>
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">Nama</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="inputName" name="nama" onChange={e => this.setState({ nama: e.target.value })} value={this.state.nama} />
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">Email</label>
+                        <div class="col-sm-9">
+                            <input type="email" class="form-control" id="inputEmail" name="email" onChange={e => this.setState({ email: e.target.value })} value={this.state.email} />
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">No. WA</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="inputNomor" name="nomor" onChange={e => this.setState({ nomor: e.target.value })} value={this.state.nomor} />
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">Jenis Kelamin</label>
+                        <div class="col-sm-9">
+                            <select class="form-select" aria-label="Default select example" name="kelamin" onChange={e => this.setState({ kelamin: e.target.value })} value={this.state.kelamin}>
+                                <option selected>jenis kelamin</option>
+                                <option value="1">Laki-Laki</option>
+                                <option value="2">Perempuan</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">Password</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" id="inputPassword" name="password" onChange={e => this.setState({ password: e.target.value })} value={this.state.password} />
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-7 offset-sm-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="gridCheck1" checked={this.state.setuju} name="setuju" onChange={e => this.setState({ setuju: e.target.checked })} />
+                                <label class="form-check-label" for="gridCheck1">
+                                    Setuju dengan kebijakan perusahaan
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <button type="submit" class="btn btn-primary ">Register</button>
+                    </div>
+                </form>
+            </div>
+            </>
+        )
+    }
+}
---
+++ src/components/Form/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "./FormInput/form";
+
+export default class FormInput extends React.Component {
+    render() {
+        return (
+            <>
+                
+                <Form />
+            </>
+        )
+    }
+}
